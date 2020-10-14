22df98652730f70ebd918c580e81623989064288
Arpit <arpit@interfacem.com>
2020-10-14 21:51:27 +0530

    Todos, TodoItem component -- In progress

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="todo-item" v-bind:class="{'done':todo.completed}">
+        <input type="checkbox" class="mr-5" v-on:change="taskDone">
         {{todo.title}}
     </div>
 </template>
@@ -7,10 +8,30 @@
 <script>
     export default {
         name: 'TodoItem',
-        props:['todo']
+        props:['todo'],
+        methods:{
+            taskDone(){
+                console.log('Task completed!');
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .todo-item{
+        background: #ffffff;
+        padding: 20px;
+        margin: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+    }
 
+    .mr-5{
+        margin-right: 5px;
+    }
+
+    .done{
+        text-decoration: line-through;
+        background: #ecebeb;
+    }
 </style>
